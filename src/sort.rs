a1237887bbf4431da885681b3fac55fa8eb20b0c
Lae Chen <laec@proton.me>
2023-06-16 08:38:13 +0000

    Add tests and fixes.

--- src/sort.rs
+++ src/sort.rs
@@ -5,7 +5,10 @@ impl Node<'_> {
   pub fn sort_by_name(&mut self) {
     match self {
       Value(_) => {}
-      Object(xs) => xs.sort_by_key(|x| x.0),
+      Object(xs) => {
+        xs.iter_mut().for_each(|(_, x)| x.sort_by_name());
+        xs.sort_by_key(|x| unquote(x.0));
+      }
       Array(xs) => xs.iter_mut().for_each(Self::sort_by_name),
     }
   }
@@ -39,5 +42,98 @@ fn unquote(s: &str) -> &str {
     &s[1..s.len() - 1]
   } else {
     s
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::Node::*;
+
+  #[test]
+  fn sort_by_name() {
+    let tests = vec![
+      (Value("1"), Value("1")),
+      (Object(vec![]), Object(vec![])),
+      (
+        Object(vec![("1", Value("a"))]),
+        Object(vec![("1", Value("a"))]),
+      ),
+      (
+        Object(vec![("1", Value("a")), ("2", Value("b"))]),
+        Object(vec![("1", Value("a")), ("2", Value("b"))]),
+      ),
+      (
+        Object(vec![("2", Value("b")), ("1", Value("a"))]),
+        Object(vec![("1", Value("a")), ("2", Value("b"))]),
+      ),
+      (
+        Object(vec![
+          ("2", Value("b")),
+          ("1", Value("a")),
+          ("3", Object(vec![("1", Value("one")), ("0", Value("zero"))])),
+        ]),
+        Object(vec![
+          ("1", Value("a")),
+          ("2", Value("b")),
+          ("3", Object(vec![("0", Value("zero")), ("1", Value("one"))])),
+        ]),
+      ),
+      (
+        Object(vec![
+          ("2", Value("b")),
+          ("1", Value("a")),
+          (
+            "3",
+            Array(vec![Object(vec![
+              ("1", Value("one")),
+              ("0", Value("zero")),
+            ])]),
+          ),
+        ]),
+        Object(vec![
+          ("1", Value("a")),
+          ("2", Value("b")),
+          (
+            "3",
+            Array(vec![Object(vec![
+              ("0", Value("zero")),
+              ("1", Value("one")),
+            ])]),
+          ),
+        ]),
+      ),
+      (Array(vec![]), Array(vec![])),
+      (
+        Array(vec![Object(vec![
+          ("1", Value("one")),
+          ("0", Value("zero")),
+        ])]),
+        Array(vec![Object(vec![
+          ("0", Value("zero")),
+          ("1", Value("one")),
+        ])]),
+      ),
+      (
+        Array(vec![Object(vec![
+          ("1", Value("one")),
+          (
+            "0",
+            Array(vec![Object(vec![("y", Value("yy")), ("x", Value("xx"))])]),
+          ),
+        ])]),
+        Array(vec![Object(vec![
+          (
+            "0",
+            Array(vec![Object(vec![("x", Value("xx")), ("y", Value("yy"))])]),
+          ),
+          ("1", Value("one")),
+        ])]),
+      ),
+    ];
+
+    for (mut actual, expected) in tests {
+      actual.sort_by_name();
+      assert_eq!(actual, expected);
+    }
   }
 }
