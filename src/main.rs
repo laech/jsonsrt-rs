16255ec85bdc5ce3f369640e667d9a3bad03f94e
Lae Chen <laec@proton.me>
2023-06-18 17:11:46 +1200

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -28,92 +28,52 @@ struct Args {
 }
 
 fn main() -> io::Result<()> {
-  run(Args::parse())
-}
-
-fn run(args: Args) -> io::Result<()> {
-  let mut input: String;
-  if let Some(path) = args.file.as_ref() {
-    input = fs::read_to_string(path)?;
-  } else {
-    input = String::new();
-    io::stdin().read_to_string(&mut input)?;
-  }
-
-  match parse(&input) {
-    Ok(mut node) => {
-      if args.sort_by_name {
-        node.sort_by_name();
-      }
-      if let Some(name) = args.sort_by_value {
-        node.sort_by_value(&name);
-      }
-
-      let output = node.to_string() + "\n";
-      if let Some(path) = args.file.as_ref() {
-        fs::write(path, output)?;
-      } else {
-        print!("{}", output)
-      }
-    }
+  let args = Args::parse();
+  match parse(&read_input(&args)?) {
     Err(e) => {
       eprintln!("{}", e);
       exit(1);
     }
-  }
 
-  Ok(())
+    Ok(mut node) => {
+      if args.sort_by_name {
+        node.sort_by_name();
+      }
+
+      if let Some(name) = args.sort_by_value.as_ref() {
+        node.sort_by_value(name);
+      }
+
+      let mut output = node.to_string();
+      output.push('\n');
+      write_output(&args, &output)?;
+
+      Ok(())
+    }
+  }
 }
 
-#[cfg(test)]
-mod arg_tests {
-  use crate::Args;
-  use clap::Parser;
-
-  #[test]
-  fn can_parse_file_arg() {
-    let args = Args::try_parse_from(["jsonsrt", "xyz"]).unwrap();
-    assert_eq!(
-      args,
-      Args {
-        sort_by_name: false,
-        sort_by_value: None,
-        file: Some("xyz".to_owned())
-      }
-    );
+fn read_input(args: &Args) -> io::Result<String> {
+  if let Some(path) = args.file.as_ref() {
+    fs::read_to_string(path)
+  } else {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    Ok(input)
   }
+}
 
-  #[test]
-  fn can_parse_sort_by_name_arg() {
-    let args = Args::try_parse_from(["jsonsrt", "--sort-by-name"]).unwrap();
-    assert_eq!(
-      args,
-      Args {
-        sort_by_name: true,
-        sort_by_value: None,
-        file: None
-      }
-    )
-  }
-
-  #[test]
-  fn can_parse_sort_by_value_arg() {
-    let args = Args::try_parse_from(["jsonsrt", "--sort-by-value", "key"]).unwrap();
-    assert_eq!(
-      args,
-      Args {
-        sort_by_name: false,
-        sort_by_value: Some("key".to_owned()),
-        file: None
-      }
-    )
+fn write_output(args: &Args, output: &str) -> io::Result<()> {
+  if let Some(path) = args.file.as_ref() {
+    fs::write(path, output)
+  } else {
+    print!("{}", output);
+    Ok(())
   }
 }
 
 #[cfg(test)]
-mod main_tests {
-  use crate::{run, Args};
-  use clap::Parser;
+mod tests {
   use std::{
     error::Error,
     fs,
@@ -139,23 +99,41 @@ mod main_tests {
   #[test]
   fn can_use_file() -> Result<(), Box<dyn Error>> {
     let mut temp = NamedTempFile::new()?;
+    let path = temp.path().to_str().unwrap().to_owned();
     temp.write(b"{ }")?;
     temp.flush()?;
 
-    let path = temp.path().to_str().unwrap().to_owned();
-    run(Args::try_parse_from(["jsonsrt", &path])?)?;
-    assert_eq!(fs::read_to_string(&path)?, "{}\n".to_owned());
+    let output = Command::new("cargo")
+      .args(["run", "--quiet", "--", &path])
+      .stdout(Stdio::piped())
+      .stderr(Stdio::piped())
+      .spawn()?
+      .wait_with_output()?;
+
+    assert_eq!("", String::from_utf8_lossy(&output.stdout).to_string());
+    assert_eq!("", String::from_utf8_lossy(&output.stderr).to_string());
+    assert!(output.status.success());
+    assert_eq!(&fs::read_to_string(&path)?, "{}\n");
     Ok(())
   }
 
   #[test]
   fn can_sort_by_name() -> Result<(), Box<dyn Error>> {
     let mut temp = NamedTempFile::new()?;
+    let path = temp.path().to_str().unwrap().to_owned();
     temp.write(r#"{"1":0,"0":0}"#.as_bytes())?;
     temp.flush()?;
 
-    let path = temp.path().to_str().unwrap().to_owned();
-    run(Args::try_parse_from(["jsonsrt", "--sort-by-name", &path])?)?;
+    let output = Command::new("cargo")
+      .args(["run", "--quiet", "--", "--sort-by-name", &path])
+      .stdout(Stdio::piped())
+      .stderr(Stdio::piped())
+      .spawn()?
+      .wait_with_output()?;
+
+    assert_eq!("", String::from_utf8_lossy(&output.stdout).to_string());
+    assert_eq!("", String::from_utf8_lossy(&output.stderr).to_string());
+    assert!(output.status.success());
     assert_eq!(
       fs::read_to_string(&path)?,
       r#"{
@@ -171,17 +149,20 @@ mod main_tests {
   #[test]
   fn can_sort_by_value() -> Result<(), Box<dyn Error>> {
     let mut temp = NamedTempFile::new()?;
+    let path = temp.path().to_str().unwrap().to_owned();
     temp.write(r#"[{"x":1},{"x":0}]"#.as_bytes())?;
     temp.flush()?;
 
-    let path = temp.path().to_str().unwrap().to_owned();
-    run(Args::try_parse_from([
-      "jsonsrt",
-      "--sort-by-value",
-      "x",
-      &path,
-    ])?)?;
+    let output = Command::new("cargo")
+      .args(["run", "--quiet", "--", "--sort-by-value", "x", &path])
+      .stdout(Stdio::piped())
+      .stderr(Stdio::piped())
+      .spawn()?
+      .wait_with_output()?;
 
+    assert_eq!("", String::from_utf8_lossy(&output.stdout).to_string());
+    assert_eq!("", String::from_utf8_lossy(&output.stderr).to_string());
+    assert!(output.status.success());
     assert_eq!(
       fs::read_to_string(&path)?,
       r#"[
